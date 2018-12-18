1ecb8aaf39136f88d6ce47c6823b0ee161687db7
Adel Senhadji <adelsenhadjii@gmail.com>
2018-12-18 18:36:12 +0100

    Update rubix.pl

--- rubix.pl
+++ rubix.pl
@@ -1,33 +1,13 @@
 rubix([A,B,C,D,E,F,G,H,I]) :- S = [A,B,C,D,E,F,G,H,I], fd_domain(S,1,9), fd_all_different(S),
 
-L1 #= A+B+C,
-L2 #= D+E+F, 
-L3 #= G+H+I,
 
-C1 #= A+D+G,
-C2 #= B+E+H, 
-C3 #= C+F+I,
-
-D1 #= A+E+I, 
-D2 #= C+E+G,
-
-L1 #= L2,
-L1 #= L3,
-L1 #= C1,
-L1 #= C2,
-L1 #= C3,
-L1 #= D1,
-L1 #= D2,
-
-/*
-L1 #= 15, 
-L2 #= 15, 
-L3 #= 15, 
-C1 #= 15, 
-C2 #= 15, 
-C3 #= 15, 
-D1 #= 15,
-D2 #= 15,
-*/
+L #= A+B+C,
+L #= D+E+F, 
+L #= G+H+I,
+L #= A+D+G,
+L #= B+E+H, 
+L #= C+F+I,
+L #= A+E+I,
+L #= C+E+G,
 
 fd_labeling(S).
